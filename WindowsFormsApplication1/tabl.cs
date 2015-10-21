4b42fe863628cc70fdbebeadd6994b991230140b
Zorik <zarkov95@mail.ru>
2015-10-21 20:42:20 +0600

    добавить класс tabl

---
+++ WindowsFormsApplication1/tabl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class tabl
+    {
+        List<string> all_lessons = new List<string>();
+        test_match t = new test_match();
+
+        public void inp(Form1 f)
+        {
+            for (int i = 2; i < 13; i++ )
+            {
+                for (int j = 1; j < 36; j++)
+                {
+                    t.testMatch(all_lessons, f.Tabl[i, j].Value.ToString());
+                    all_lessons.Add(f.Tabl[i, j].Value.ToString());
+                    Console.WriteLine(f.Tabl[i, j].Value.ToString());
+                }
+            }
+        }
+    }
+}
